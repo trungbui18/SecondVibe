53b908dd14898073e887bd0d0e06811e6d548a44
trungbui18 <trungbui512003@gmail.com>
2025-05-24 10:04:46 +0700

    Initial commit

---
+++ secondvibe_backend/src/main/java/org/example/secondvibe_backend/SecondVibeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.secondvibe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondVibeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondVibeBackendApplication.class, args);
+    }
+
+}
